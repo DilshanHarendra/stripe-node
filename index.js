9483c94fc18c33b330ce39cc07f563fd5d3bf2b2
DilshanHarendra <harendracripto@gmail.com>
2022-05-26 23:57:15 +0530

    create simple app

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors= require('cors')
 const bodyParser = require('body-parser')
+const {response} = require("express");
 app.use(cors())
 app.use(bodyParser())
 app.use(express.static(`${__dirname}/public`))
@@ -14,31 +15,64 @@ const storeItems = new Map([
     [2, { priceInCents: 20000, name: "Learn CSS Today" }],
 ])
 
-console.log(storeItems)
+
 
 
 app.get('/',(req, res) => {
     res.sendFile('index.html')
 })
 
+app.post("/product",(req,res)=>{
+    let productData={
+       // "id": "prod_LiHvCbwIEq59lT",
+        name: "Frock Red Black",
+        default_price_data:{
+            unit_amount: 100,
+            currency: 'usd',
+          //  recurring: {interval: 'month'},
+
+        },
+       // "active": true,
+        description: "This is a test product. This is a red color frock u can buy this.",
+        images: [
+            "https://images.unsplash.com/photo-1545912452-8aea7e25a3d3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGdpcmx8ZW58MHx8MHx8&auto=format&fit=crop&w=800",
+            "https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGdpcmx8ZW58MHx8MHx8&auto=format&fit=crop&w=800"
+        ],
+        package_dimensions: {
+            height:2.00,
+            width:20.00,
+            length:12.00,
+            weight:50.00
+        },
+        shippable: true,
+       // "statement_descriptor": null,
+        tax_code: "txcd_00000000",
+        unit_label: "Pcs",
+        url: "http://localhost:8000/product/1",
+       // object: "product",
+       // metadata: {},
+    }
+
+    stripe.products.create(productData).then(response=>{
+        res.status(200).send(response)
+    }).catch(err=>res.status(400).send(err))
+
+})
+
+
+
 app.post("/create-checkout-session", async (req, res) => {
     try {
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ["card"],
-            mode: "payment",
-            line_items: req.body.items.map(item => {
-                const storeItem = storeItems.get(item.id)
-                return {
-                    price_data: {
-                        currency: "usd",
-                        product_data: {
-                            name: storeItem.name,
-                        },
-                        unit_amount: storeItem.priceInCents,
-                    },
-                    quantity: item.quantity,
+            mode: 'payment',
+            line_items: [
+                {
+                    price:'price_1L2gLnKfS07LkwswM0ERACcW',
+                    quantity:req.body.qty||1
                 }
-            }),
+            ],
+
             success_url: `${process.env.CLIENT_URL}/success.html`,
             cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
         })
@@ -53,4 +87,4 @@ app.get('/',(req, res) => {
 })
 
 
-app.listen(8000,()=>console.log("running on 8000"))
+app.listen(8000,()=>console.log("running on http://localhost:8000"))
