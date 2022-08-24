9a2ae98a1335d80ed30b170e6aac7d2fa7d849ed
DilshanHarendra <harendracripto@gmail.com>
2022-08-24 11:08:31 +0530

    few change

--- index.js
+++ index.js
@@ -8,13 +8,84 @@ app.use(cors())
 app.use(bodyParser())
 app.use(express.static(`${__dirname}/public`))
 const stripe =require("stripe")(process.env.STRIPE_KEY)
+const port =8000
 
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret = "whsec_93a109d78a298a6e27db80b377367b817715238c12e240e549c72993662aa024";
 
-const storeItems = new Map([
-    [1, { priceInCents: 10000, name: "Learn React Today" }],
-    [2, { priceInCents: 20000, name: "Learn CSS Today" }],
-])
-
+const PRICE_IDS={
+    ONETIME:'price_1L7jhYKfS07LkwswD62k0sqt',
+    MONTHLY:'price_1L7jhYKfS07LkwswrdKNjYMi',
+    YEARLY:'price_1L7jhYKfS07Lkwswax1J90ME'
+}
+const  PERMISSIONS= {
+    "ONETIME":[
+        {
+            "listings": 3,
+            "discount": 0,
+            "coupon": null
+        },
+        {
+            "listings": 6,
+            "discount": 8.33,
+            "coupon": "F00xloU1"
+        },
+        {
+            "listings": 50,
+            "discount": 16.67,
+            "coupon": "rhu2zdUM"
+        },
+        {
+            "listings": 1000,
+            "discount": 25,
+            "coupon": "Mm1TChHM"
+        }
+    ],
+    "MONTHLY": [
+        {
+            "listings": 5,
+            "discount": 0,
+            "coupon": null
+        },
+        {
+            "listings": 20,
+            "discount": 10,
+            "coupon": "Q7Z9leK5"
+        },
+        {
+            "listings": 100,
+            "discount": 20,
+            "coupon": "M9urjnPI"
+        },
+        {
+            "listings": 1000,
+            "discount": 30,
+            "coupon": "smRDBMKX"
+        }
+    ],
+    "YEARLY": [
+        {
+            "listings": 5,
+            "discount": 8.33,
+            "coupon": "aJ5XmuAT"
+        },
+        {
+            "listings": 20,
+            "discount": 26.72,
+            "coupon": "qWNWcLgH"
+        },
+        {
+            "listings": 100,
+            "discount": 20,
+            "coupon": "os9NyQXc"
+        },
+        {
+            "listings": 1000,
+            "discount": 35.83,
+            "coupon": "i7WZuMXb"
+        }
+    ]
+}
 
 
 
@@ -63,164 +134,61 @@ app.post("/product",(req,res)=>{
 
 app.post("/create-checkout-session", async (req, res) => {
     try {
-        const {freq, packageName, qty}= req.body
+        const {freq, qty}= req.body
 
        /*
        #NOTE
        freq can be ONE, MONTHLY, YEARLY
-       packageName can be BASIC, STANDARD , CUSTOM
        */
 
-        const priceIds={
-            ONETIME:'price_1L7jhYKfS07LkwswD62k0sqt',
-            MONTHLY:'price_1L7jhYKfS07LkwswrdKNjYMi',
-            YEARLY:'price_1L7jhYKfS07Lkwswax1J90ME'
-        }
-        const coupons={
-            ONETIME_PKG_2:'F00xloU1',
-            ONETIME_PKG_3_V1:'rhu2zdUM',
-            ONETIME_PKG_3_V2:'Mm1TChHM',
-            SUBSCRIPTION_PKG_2_MONTHLY:'Q7Z9leK5',
-            SUBSCRIPTION_PKG_3_V1_MONTHLY:'M9urjnPI',
-            SUBSCRIPTION_PKG_3_V2_MONTHLY:'smRDBMKX',
-            SUBSCRIPTION_PKG_1_YEARLY:'aJ5XmuAT',
-            SUBSCRIPTION_PKG_2_YEARLY:'qWNWcLgH',
-            SUBSCRIPTION_PKG_3_V1_YEARLY:'os9NyQXc',
-            SUBSCRIPTION_PKG_3_V2_YEARLY:'i7WZuMXb'
-        }
-        const getQty=()=>{
-            switch (packageName){
-                case 'BASIC':{
-                    if (freq=='ONE'){
-                        return 3
-                    }else{
-                        return 5
-                    }
-                }
-                case 'STANDARD':{
-                    if (freq=='ONE'){
-                        return 6
-                    }else{
-                        return 20
-                    }
-                }
-                default : return qty
-            }
-        }
+
+
         let sessionObj= {
             payment_method_types: ["card"],
-            mode: freq=='ONE'?'payment':'subscription',
-            success_url: `${process.env.CLIENT_URL}/success.html`,
-            cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
+            metadata: {
+                            qty:qty,
+                            interval:freq,
+                        },
+            mode: freq=='ONETIME'?'payment':'subscription',
+            success_url: `http://localhost:${port}/success.html`,
+            cancel_url: `http://localhost:${port}/cancel.html`,
         }
 
+        const priceID = PRICE_IDS[freq]
 
 
-        if (freq=='ONE'){
-            const line_item={
-                price:priceIds.ONETIME,
-                quantity:getQty(),
+
+        if (priceID==""){
+            throw "Missing Price id"
+        }
+
+        let permissions  = PERMISSIONS[freq]
+        permissions= permissions.sort((a,b)=>a.listings>b.listings?1:-1)
+        let coupon=null
+
+        const line_item={  // create line items
+            price:priceID,
+            quantity:qty,
+        }
+        sessionObj={  // add items
+            ...sessionObj,
+            line_items: [line_item],
+        }
+        for (let i = 0; i < permissions.length; i++) {  // find coupon
+            if (permissions[i].listings>=qty){
+                coupon=permissions[i].coupon
+                break
             }
+        }
 
-            if (packageName=='CUSTOM'){
-                if (qty<7){
-                   throw "Quantity must grater than 7"
-                }
-                let discount = coupons.ONETIME_PKG_3_V1
-                if (qty>50){
-                    discount=coupons.ONETIME_PKG_3_V2
-                }
-                sessionObj={
-                    ...sessionObj,
-                    line_items: [line_item],
-                    discounts: [{
-                        coupon: discount,
-                    }],
-                }
-
-            }else if(packageName=='STANDARD') {
-                sessionObj={
-                    ...sessionObj,
-                    line_items: [line_item],
-                    discounts: [{
-                        coupon: coupons.ONETIME_PKG_2,
-                    }],
-                }
-            }else{
-                sessionObj={
-                    ...sessionObj,
-                    line_items: [line_item],
-                }
-
-            }
-
-        }else if (freq=='MONTHLY'){
-            const line_item={
-                price:priceIds.MONTHLY,
-                quantity:getQty(),
-            }
-            let discount = coupons.SUBSCRIPTION_PKG_2_MONTHLY
-            if(packageName=='CUSTOM'){
-                if (qty<21){
-                    throw "Quantity must grater than 21"
-                }
-                if (qty<101){
-                    discount=coupons.SUBSCRIPTION_PKG_3_V1_MONTHLY
-                }else {
-                    discount=coupons.SUBSCRIPTION_PKG_3_V2_MONTHLY
-                }
-                sessionObj={
-                    ...sessionObj,
-                    line_items: [line_item],
-                    discounts: [{
-                        coupon: discount,
-                    }],
-                }
-            }else if(packageName=='STANDARD') {
-                sessionObj={
-                    ...sessionObj,
-                    line_items: [line_item],
-                    discounts: [{
-                        coupon: coupons.SUBSCRIPTION_PKG_2_MONTHLY,
-                    }],
-                }
-            }else{
-                sessionObj={
-                    ...sessionObj,
-                    line_items: [line_item],
-                }
-            }
-
-        }else if (freq=='YEARLY'){
-            const line_item={
-                price:priceIds.YEARLY,
-                quantity:getQty()*12,
-            }
-            let discount = coupons.SUBSCRIPTION_PKG_1_YEARLY
-            if(packageName=='CUSTOM'){
-                if (qty<21){
-                    throw "Quantity must grater than 21"
-                }
-                if (qty<101){
-                    discount=coupons.SUBSCRIPTION_PKG_3_V1_YEARLY
-                }else {
-                    discount=coupons.SUBSCRIPTION_PKG_3_V2_YEARLY
-                }
-            }else if(packageName=='STANDARD') {
-                discount=coupons.SUBSCRIPTION_PKG_2_YEARLY
-            }
+        if (coupon){
             sessionObj={
                 ...sessionObj,
-                line_items: [line_item],
                 discounts: [{
-                    coupon: discount,
+                    coupon: coupon,
                 }],
             }
-
-        }else {
-            throw 'freq must be ONE, MONTHLY, YEARLY'
         }
-
 
         const session = await stripe.checkout.sessions.create(sessionObj)
         res.json({ url: session.url })
@@ -230,12 +198,127 @@ app.post("/create-checkout-session", async (req, res) => {
     }
 })
 
+
 app.get('/price',(req, res) => {
     stripe.prices.retrieve(
-        'price_1L5CwFDm5VBUQrEut5cxyHmi'
+        'price_1L7EpmDm5VBUQrEuVMOpddZY'
     ).then(response=>res.json({data:response}))
         .catch(err=>res.json({data:err}))
 })
 
+app.get('/billing/:id',async (req, res) => {
+    console.log(req.params.id)
+    const session = await stripe.billingPortal.sessions.create({
+        customer: req.params.id||'',
+        return_url: `http://localhost:${port}/index.html`,
+        }
+    )
+    res.json({ data: session })
+})
+/*
+onetime->onetime   // checkout api
+onetime->subscription // checkout api
+subscription->subscription //
+subscription->onetime
+* */
 
-app.listen(8000,()=>console.log("running on http://localhost:8000"))
+app.post("/change-subscription", async (req, res) => {
+    try {
+        const {freq, qty}= req.body
+        // TODO : get currentSubscription
+
+
+        // TODO :
+
+
+
+        const subscriptionID="sub_1LAe2BKfS07LkwswpSMFGVPo"
+        const subscriptionData = await stripe.subscriptions.retrieve(subscriptionID);
+        console.log(subscriptionData)
+        console.log(subscriptionData.items.data)
+        const priceID=PRICE_IDS[freq]
+        let permissions  = PERMISSIONS[freq]
+        permissions= permissions.sort((a,b)=>a.listings>b.listings?1:-1)
+        let coupon=null
+        let sessionObj={
+            proration_behavior: 'always_invoice',
+            metadata:{
+
+            }
+        }
+
+        let product={
+            quantity:qty,
+            id: subscriptionData.items.data[0].id,
+            price: priceID,
+        }
+        sessionObj={
+            ...sessionObj,
+            items:[product],
+        }
+        for (let i = 0; i < permissions.length; i++) {  // find coupon
+            if (permissions[i].listings>=qty){
+                coupon=permissions[i].coupon
+                break
+            }
+        }
+
+        if (coupon){
+            sessionObj={
+                ...sessionObj,
+                coupon: coupon,
+            }
+        }
+
+
+        const newSubscription = await stripe.subscriptions.update(
+            subscriptionID,
+            sessionObj
+        );
+
+        res.status(200).send(newSubscription)
+
+    }catch (e){
+        res.status(400).json({ error: e })
+    }
+})
+
+
+
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+    const sig = request.headers['stripe-signature'];
+
+    let event;
+
+    try {
+        event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+        response.status(400).send(`Webhook Error: ${err.message}`);
+        return;
+    }
+
+    console.log(event)
+    // Handle the event
+    switch (event.type) {
+        case 'payment_intent.succeeded':
+            const paymentIntent = event.data.object;
+            // Then define and call a function to handle the event payment_intent.succeeded
+            break;
+        // ... handle other event types
+        default:
+            console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+});
+
+
+app.get('/clock',(req,res)=>{
+
+})
+
+
+
+app.listen(port,()=>console.log(`running on http://localhost:${port}`))
+
